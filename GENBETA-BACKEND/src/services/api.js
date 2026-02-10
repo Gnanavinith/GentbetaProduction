3d683d059a81d96ab349bc870228912a2830456c
Aravindhady <varavind746@gmail.com>
2026-02-10 18:26:50 +0530

    update the base url code

--- GENBETA-BACKEND/src/services/api.js
+++ GENBETA-BACKEND/src/services/api.js
@@ -1,5 +1,5 @@
 export const API = (url, data, token) =>
-  fetch(`${process.env.API_BASE_URL || 'http://localhost:5001'}${url}`, {
+  fetch(`${process.env.API_BASE_URL || 'https://app.matapangtech.com'}${url}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
